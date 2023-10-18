b6259b8d14c96377474b00e40076dd78578747e6
Gabriel de Melo Marcondes <gabriel-santos1313@hotmail.com>
2023-10-18 11:26:47 -0300

    Updated with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Gear } from '@phosphor-icons/react'
 
 import './styles.scss'
@@ -6,50 +7,56 @@ function App() {
   const duration = '3s'
 
   return (
-    <main>
-      <div className="box">
-        <h1>Em manutenção</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={
+          <main>
+            <div className="box">
+              <h1>Em manutenção</h1>
 
-        <div className="gears animate-type-2">
-          <div className="g gear-1">
-            <Gear size={68} weight='duotone' className='gear'>
-              <animateTransform
-                attributeName='transform'
-                type='rotate'
-                dur={duration}
-                from='0 0 0'
-                to='360 0 0'
-                repeatCount='indefinite'
-              />
-            </Gear>
-          </div>
-          <div className="g gear-2">
-            <Gear size={68} weight='duotone' className='gear'>
-              <animateTransform
-                attributeName='transform'
-                type='rotate'
-                dur={duration}
-                from='0 0 0'
-                to='-360 0 0'
-                repeatCount='indefinite'
-              />
-            </Gear>
-          </div>
-          <div className="g gear-3">
-            <Gear size={68} weight='duotone' className='gear'>
-              <animateTransform
-                attributeName='transform'
-                type='rotate'
-                dur={duration}
-                from='0 0 0'
-                to='360 0 0'
-                repeatCount='indefinite'
-              />
-            </Gear>
-          </div>
-        </div>
-      </div>
-    </main>
+              <div className="gears animate-type-2">
+                <div className="g gear-1">
+                  <Gear size={68} weight='duotone' className='gear'>
+                    <animateTransform
+                      attributeName='transform'
+                      type='rotate'
+                      dur={duration}
+                      from='0 0 0'
+                      to='360 0 0'
+                      repeatCount='indefinite'
+                    />
+                  </Gear>
+                </div>
+                <div className="g gear-2">
+                  <Gear size={68} weight='duotone' className='gear'>
+                    <animateTransform
+                      attributeName='transform'
+                      type='rotate'
+                      dur={duration}
+                      from='0 0 0'
+                      to='-360 0 0'
+                      repeatCount='indefinite'
+                    />
+                  </Gear>
+                </div>
+                <div className="g gear-3">
+                  <Gear size={68} weight='duotone' className='gear'>
+                    <animateTransform
+                      attributeName='transform'
+                      type='rotate'
+                      dur={duration}
+                      from='0 0 0'
+                      to='360 0 0'
+                      repeatCount='indefinite'
+                    />
+                  </Gear>
+                </div>
+              </div>
+            </div>
+          </main>
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
